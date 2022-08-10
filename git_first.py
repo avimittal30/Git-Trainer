94261d91a82c66f0b7064d098aada8c852c4683b
Aviral <avimittal30@gmail.com>
2022-08-10 17:29:54 +0530

    initial commit:

---
+++ git_first.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 10 17:25:02 2022
+
+@author: aviral.mittal
+"""
+
+
+db_age={'A':32,'B':26, 'C':40, 'D':45, 'E':37}
+db_age.update({'F':39})
+print(db_age['F'])
